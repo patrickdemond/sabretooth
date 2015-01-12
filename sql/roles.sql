1c2e0cf72e7ad306fba5b4796d8a1ee3140dc466
Patrick Emond <emondpd@mcmaster.ca>
2015-01-12 12:04:17 -0500

    Adding missing operation to roles.sql script

--- sql/roles.sql
+++ sql/roles.sql
@@ -853,6 +853,11 @@ WHERE type = "push" AND subject = "participant" AND operation.name = "edit"
 AND role.name IN( "administrator", "curator", "helpline", "operator", "supervisor" );
 
 INSERT INTO role_has_operation( role_id, operation_id )
+SELECT role.id, operation.id FROM role, operation
+WHERE type = "widget" AND subject = "participant" AND operation.name = "hin"
+AND role.name IN( "administrator", "operator", "supervisor" );
+
+INSERT INTO role_has_operation( role_id, operation_id )
 SELECT role.id, operation.id FROM cenozo.role, operation
 WHERE type = "pull" AND subject = "participant" AND operation.name = "list"
 AND role.name IN( "administrator", "curator", "helpline", "supervisor" );
