314af94f7fd0106dff35c161409ea7dcdd7ef767
Patrick Emond <emondpd@mcmaster.ca>
2015-09-10 14:47:31 -0400

    Changing voip recording logic
    The software no longer depends on recording files being stored locally.

--- api/ui/push/voip_begin_monitor.class.php
+++ api/ui/push/voip_begin_monitor.class.php
@@ -41,9 +41,21 @@ class voip_begin_monitor extends \cenozo\ui\push
     // get the filename by creating a temporary recording with the current interview/assignment ids
     $token = lib::create( 'business\survey_manager' )->get_current_token();
     $parts = explode( '_', $token );
+
+    // get the highest ranking recording for this interview
+    $modifier = lib::create( 'database\modifier' );
+    $modifier->where( 'interview_id', '=', $parts[0] );
+    $max_rank = lib::create( 'business\session' )->get_database()->get_one(
+      sprintf( 'SELECT MAX( rank ) FROM recording %s', $modifier->get_sql() ) );
+
+    $rank = is_null( $max_rank ) ? 1 : $max_rank + 1;
+
     $db_recording = lib::create( 'database\recording' );
     $db_recording->interview_id = $parts[0];
     $db_recording->assignment_id = $parts[1];
+    $db_recording->rank = $rank;
+    $db_recording->save();
+
     $filename = $db_recording->get_filename();
     lib::create( 'business\voip_manager' )->get_call()->start_monitoring( $filename );
   }
